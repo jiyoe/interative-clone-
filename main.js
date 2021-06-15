8b6fa879e04ed6fc64684d9297d4e4d4bc2c5fb0
jiyoe <ejdgodl@naver.com>
2021-06-15 23:44:01 +0900

    add js 1

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 (() => {
-    const
-}
+
+    const stepElems = document.querySelectorAll('.step');
+    const graphicElems = document.querySelectorAll('.graphic-item');
+
+    for ( let i = 0; i < stepElems.length; i++) {
+        //데이터 인덱스 번호만들어주는 함수
+        // stepElems[i].setAttribute('data-index', i);
+        stepElems[i].dataset.index = i;
+        graphicElems[i].dataset.index = i;
+    }
+
+})();
