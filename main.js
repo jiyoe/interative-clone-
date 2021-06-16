e3ee4dc19a0183d309a4e49dc2966d8c40519bc0
jiyoe <ejdgodl@naver.com>
2021-06-16 13:17:52 +0900

    함수화, currentItem = graphicElems[0] 첫이미지 설정추가

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
     const stepElems = document.querySelectorAll('.step');
     const graphicElems = document.querySelectorAll('.graphic-item');
     //현재활성화된(visible 클래스가 붙은) .graphic-item을 저장
-    let currentItem;
+    let currentItem = graphicElems[0];
 
     for ( let i = 0; i < stepElems.length; i++) {
         //데이터 인덱스 번호만들어주는 함수
@@ -12,6 +12,14 @@
         graphicElems[i].dataset.index = i;
     }
     
+    function activate() {
+        currentItem.classList.add('visible');
+    }
+
+    function inactivate() {
+        currentItem.classList.remove('visible');
+    }
+
     window.addEventListener('scroll', () =>{
         let step;
         let boundingRect;
@@ -23,12 +31,13 @@
             if (boundingRect.top > window.innerHeight * 0.1 &&
                 boundingRect.top < window.innerHeight * 0.8) {
 
-                    if(currentItem){
-                        currentItem.classList.remove('visible');
-                    }
+                    inactivate();
                     currentItem = graphicElems[step.dataset.index];
-                    currentItem.classList.add('visible');
+                    activate();
                 }
         }
     });
+
+    activate();
+
 })();
